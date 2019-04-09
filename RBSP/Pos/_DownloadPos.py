e4d8c66ce395c951f1cd582c2347cbbf2dc903d1
Matt <mattkjames7@gmail.com>
2019-04-09 11:32:38 +0100

    fixed position data download

--- RBSP/Pos/_DownloadPos.py
+++ RBSP/Pos/_DownloadPos.py
@@ -6,8 +6,8 @@ def _DownloadPos():
 	Downloads position data and extracts it to RBSP_PATH/Pos. 
 	'''
 
-	arch = ['https://github.com/mattkjames7/RBSP/blob/master/DATA/rbspa.bz2',
-			'https://github.com/mattkjames7/RBSP/blob/master/DATA/rbspa.bz2']
+	arch = ['https://github.com/mattkjames7/RBSP/raw/master/DATA/rbspa.bin.bz2',
+			'https://github.com/mattkjames7/RBSP/raw/master/DATA/rbspa.bin.bz2']
 	
 	#firstly, let's check what exists
 	Path = Globals.DataPath+'Pos/'
@@ -17,10 +17,9 @@ def _DownloadPos():
 	data_exists = [os.path.isfile(Path+fname[0]+'.bin'),
 					os.path.isfile(Path+fname[1]+'.bin')]
 	
-	arch_exists =  [os.path.isfile(Path+fname[0]+'.bz2'),
-					os.path.isfile(Path+fname[1]+'.bz2')]
+	arch_exists =  [os.path.isfile(Path+fname[0]+'.bin.bz2'),
+					os.path.isfile(Path+fname[1]+'.bin.bz2')]
 	
-	print(data_exists,arch_exists)
 
 	#create the directory if needed
 	if not path_exists:
@@ -30,13 +29,13 @@ def _DownloadPos():
 		if not data_exists[i] and not arch_exists[i]:
 			#neither data nor archive exists, download then extract
 			print('Downloading position data for {:s}...'.format(fname[i]))
-			os.system('wget '+arch[i]+' -O '+Path+fname[i]+'.bz2')
+			os.system('wget '+arch[i]+' -O '+Path+fname[i]+'.bin.bz2')
 			print('Extracting archive...')
-			os.system('bunzip2 '+Path+fname[i]+'.bz2')
+			os.system('bunzip2 '+Path+fname[i]+'.bin.bz2')
 		elif arch_exists[i] and not data_exists[i]:
 			#just need to extract the archive
 			print('Extracting archive...')
-			os.system('bunzip2 '+Path+fname[i]+'.bz2')
+			os.system('bunzip2 '+Path+fname[i]+'.bin.bz2')
 		else:
 			#data must already exist
 			print('Data for {:s} already exists, nothing to do'.format(fname[i]))			
